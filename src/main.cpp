2348457462e11170acdb8dddb5ef8b8e0b73dc8e
nilswag <nilswagemaker05@icloud.com>
2026-06-18 15:32:28 +0200

    Properly passed parameter pack

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,19 @@
 
 int main()
 {
-	logger::trace("Hello World!");
-	logger::debug("Hello World!");
-	logger::info("Hello World!");
-	logger::warn("Hello World!");
-	logger::error("Hello World!");
-	logger::fatal("Hello World!");
+	try
+	{
+		logger::trace("Hello World!");
+		logger::debug("Hello World!");
+		logger::info("Hello World!");
+		logger::warn("Hello World!");
+		logger::error("Hello World!");
+		logger::fatal("Hello World!");
+	}
+	catch (const std::exception& e)
+	{
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
--- src/util/log.h
+++ src/util/log.h
@@ -24,48 +24,48 @@ namespace logger
                case LogLevel::FATAL: prefix = "\033[0;31m[FATAL]"; break;
                case LogLevel::ERROR: prefix = "\033[0;31m[ERROR]"; break;
                case LogLevel::WARN:  prefix = "\033[0;33m[WARN]";  break;
-               case LogLevel::INFO:  prefix = "\033[1;37m[INFO]";  break;
+               case LogLevel::INFO:  prefix = "\033[0;37m[INFO]";  break;
                case LogLevel::DEBUG: prefix = "\033[0;36m[DEBUG]"; break;
                case LogLevel::TRACE: prefix = "\033[0;35m[TRACE]"; break;
           }
 
-          std::println("{:4} {}\033[0m", prefix, std::format(msg, std::forward<Args>(args)...));
+          std::println("{}\t{}\033[0m", prefix, std::format(msg, std::forward<Args>(args)...));
      }
 
      template<typename... Args>
      inline void fatal(const std::format_string<Args...> msg, Args&&... args)
      {
-          out(LogLevel::FATAL, msg, std::forward(args));
+          out(LogLevel::FATAL, msg, args...);
           throw std::exception();
      }
 
      template<typename... Args>
      inline void error(const std::format_string<Args...> msg, Args&&... args)
      {
-          out(LogLevel::ERROR, msg, std::forward(args));
+          out(LogLevel::ERROR, msg, args...);
      }
 
      template<typename... Args>
      inline void warn(const std::format_string<Args...> msg, Args&&... args)
      {
-          out(LogLevel::WARN, msg, std::forward(args));
+          out(LogLevel::WARN, msg, args...);
      }
 
      template<typename... Args>
      inline void info(const std::format_string<Args...> msg, Args&&... args)
      {
-          out(LogLevel::INFO, msg, std::forward(args));
+          out(LogLevel::INFO, msg, args...);
      }
 
      template<typename... Args>
      inline void debug(const std::format_string<Args...> msg, Args&&... args)
      {
-          out(LogLevel::DEBUG, msg, std::forward(args));
+          out(LogLevel::DEBUG, msg, args...);
      }
 
      template<typename... Args>
      inline void trace(const std::format_string<Args...> msg, Args&&... args)
      {
-          out(LogLevel::TRACE, msg, std::forward(args));
+          out(LogLevel::TRACE, msg, args...);
      }
 }

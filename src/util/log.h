63bea1c0ff1198dd2f5376a46855b9c718fd496b
nilswag <nilswagemaker05@icloud.com>
2026-06-18 14:52:38 +0200

    Added log declarations

--- src/util/log.h
+++ src/util/log.h
@@ -1,3 +1,52 @@
 #pragma once
+#include <string>
+#include <print>
 
-     
+namespace log
+{
+     enum class LogLevel
+     {
+          FATAL,
+          ERROR,
+          WARN,
+          INFO,
+          DEBUG,
+          TRACE
+     }
+
+     template<typename T, Args... args>
+     inline void log(LogLevel& level, const std::string& fmt, Args... args)
+     {
+          
+     }
+
+     inline void fatal()
+     {
+
+     }
+
+     inline void error()
+     {
+
+     }
+
+     inline void warn()
+     {
+
+     }
+
+     inline void info()
+     {
+
+     }
+
+     inline void debug()
+     {
+
+     }
+
+     inline void trace()
+     {
+          
+     }
+}

b33c29ea47d18eaf485678a35bc82fb3ce6283dc
nilswag <nilswagemaker05@icloud.com>
2026-06-26 00:30:21 +0200

    Added method for fetching queue family

--- src/vulkan/vk_device.cpp
+++ src/vulkan/vk_device.cpp
@@ -4,7 +4,7 @@
 #include "vk_instance.h"
 #include "../util/log.h"
 
-Device Device::get_device(Instance& instance, uint32_t index)
+Device Device::get_device(Instance& instance, uint32_t index, const std::vector<const char*>& requested_device_extensions)
 {
     logger::trace("Device: creating device with index {}", index);
 
@@ -16,10 +16,10 @@ Device Device::get_device(Instance& instance, uint32_t index)
     if (index >= physical_devices.size())
         logger::fatal("Device: index out of bounds");
 
-    return Device(instance, physical_devices[index]);
+    return Device(instance, physical_devices[index], requested_device_extensions);
 }
 
-Device::Device(Instance& instance, VkPhysicalDevice& physical_device)
+Device::Device(Instance& instance, VkPhysicalDevice& physical_device, const std::vector<const char*>& requested_device_extensions)
     : physical_device(physical_device)
 {
     logger::debug("Device: initializing");
@@ -32,4 +32,33 @@ Device::Device(Instance& instance, VkPhysicalDevice& physical_device)
     logger::info("Device: using '{}'", physical_device_properties.properties.deviceName);
 
     logger::debug("Device: initialized");
+}
+
+void Device::create_queue()
+{
+    logger::trace("Device: creating queue");
+
+    uint32_t queue_family_count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, nullptr);
+    std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, queue_families.data());
+
+    uint32_t queue_family = 0;
+    for (size_t i = 0; i < queue_families.size(); i++)
+    {
+        if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            queue_family = i;
+            break;
+        }
+    }
+    if (queue_family == 0)
+        logger::fatal("Device: queue family unsupported 'VK_QUEUE_GRAPHICS_BIT'");
+    
+    const float queue_priorities = 1.0f;
+    VkDeviceQueueCreateInfo queue_ci = {};
+    queue_ci.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queue_ci.queueFamilyIndex = queue_family;
+    queue_ci.queueCount = 1;
+    queue_ci.pQueuePriorities = &queue_priorities;
 }
--- src/vulkan/vk_device.h
+++ src/vulkan/vk_device.h
@@ -5,12 +5,14 @@
 class Device
 {
 public:
-    static Device get_device(Instance& instance, uint32_t index);
+    static Device get_device(Instance& instance, uint32_t index, const std::vector<const char*>&);
 
 private:
-    Device(Instance& instance, VkPhysicalDevice& physical_device);
+    Device(Instance& instance, VkPhysicalDevice& physical_device, const std::vector<const char*>&);
+    void create_queue();
 
     VkPhysicalDevice physical_device;
     VkDevice logical_device;
+
 
 };
